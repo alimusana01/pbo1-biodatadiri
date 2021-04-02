b43471c49c4bac97de8d627aa7970851c12dd5a2
alimusana01 <alimusana68@gmail.com>
2021-04-02 09:16:34 +0700

    buat input dan output

--- biodatadiri.java
+++ biodatadiri.java
@@ -4,5 +4,23 @@ public class biodatadiri{
  public static void main (String [] args){
  Scanner input = new Scanner(System.in);
 
+ String nim, nama, klas, jurusan;
+
+ ystem.out.print("nim : ");
+ nim = input.nextLine();
+
+ System.out.print("nama : ");
+ nama = input.nextLine();
+
+ System.out.print("klas : ");
+ klas = input.nextLine();
+
+ System.out.print("jurusan : ");
+ jurusan = input.nextLine();
+
+	System.out.println(" Nim anda adalah 	:"+nim);
+	System.out.println(" Nama anda adalah	:"+nama);
+	System.out.println(" Klas anda adalah	:"+klas);
+	System.out.println(" Jurusan anda adalah :"+jurusan);
 
 }
